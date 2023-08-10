70c43fdbb74dce0b387c8e8b459f89f91778fee3
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2023-08-10 16:37:38 +0530

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
+import Appbar from './Appbar'
 import Signup from './Signup'
+import Signin from './Signin'
+import AddCourse from './AddCourse'
 
 function App() {
-  
+
   return (
-    <>
-    <div style={{width:"100vw",height:"100vh",background:'#eeeeee'}}>
-  <Signup></Signup>
-  </div>
-   
-    </>
+
+    <div style={{ width: "100vw", height: "100vh", background: '#eeeeee' }}>
+
+      <Router>
+        <Appbar />
+
+        <Routes>
+          <Route path={"/addcourse"} element={<AddCourse />} />
+          <Route path="/login" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+
   )
 }
 
